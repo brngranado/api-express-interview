edd17dcbd0168a19c87ee372728e77d8278eeabc
Bryan Granado <68772240+brngranado@users.noreply.github.com>
2024-06-05 21:43:18 -0400

    Update db.config.ts

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -13,24 +13,4 @@ export class FirestoreDb {
   public connect = async () => {
     return await this.db;
   };
-
-  //   public async getCollection(collectionName: string) {
-  //     return this.db.collection(collectionName);
-  //   }
-
-  //   public async getDocument(collectionName: string, documentId: string) {
-  //     return this.db.collection(collectionName).doc(documentId);
-  //   }
-
-  //   public async addDocument(collectionName: string, data: any) {
-  //     return this.db.collection(collectionName).add(data);
-  //   }
-
-  //   public async updateDocument(collectionName: string, documentId: string, data: any) {
-  //     return this.db.collection(collectionName).doc(documentId).update(data);
-  //   }
-
-  //   public async deleteDocument(collectionName: string, documentId: string) {
-  //     return this.db.collection(collectionName).doc(documentId).delete();
-  //   }
 }
